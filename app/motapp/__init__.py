76950175e4508620ca6c384a12a7c4ee6b76db7f
johnnylord <johnnylord7410@gmail.com>
2020-09-10 10:36:36 +0800

    Add motapp

---
+++ app/motapp/__init__.py
@@ -0,0 +1,151 @@
+import logging
+
+import cv2
+import numpy as np
+
+from ..base import App
+from ..base import Keyboard as kb
+from ..gui.container import check_ready
+from ..gui.media import MediaType
+from ..utils.transform import convert_bbox_coordinate
+from ..utils.visualize import draw_bbox, draw_text, draw_gaussian, get_unique_color
+
+
+logger = logging.getLogger(__name__)
+
+
+__all__ = [ "MOTApp" ]
+
+class MOTApp(App):
+
+    MATCHED_WORKER = "MOTWorker"
+
+    def __init__(self, **kwargs):
+        raise RuntimeError("Cannot directly instantiate object from MOTApp")
+
+    def boot(self):
+        """Prepare runtime environment for worker"""
+        self.event_handler = { 'track': self._track_handler }
+
+    @check_ready
+    def run(self):
+        """App loop for running app"""
+        while not self.is_stop():
+            content = self.render()
+            fid, frame = content['fid'], content['container_frame']
+
+            # Send request
+            request = { 'action': 'track' }
+            self.send(request)
+
+            # Send raw frames to workers
+            video_frames = []
+            for panel in self.panel_to_channel.keys():
+                media_frame = panel.media_cache
+                media_frame = cv2.resize(media_frame, self.trans_resolution)
+                frame_bytes = cv2.imencode('.jpg', media_frame)[1]
+                video_frames.append({ 'panel': panel, 'frame_bytes': frame_bytes })
+            self.parallel_send_videos(video_frames)
+
+            # Catch response from remote worker
+            response = self.recv()
+            if response is None:
+                break
+
+            # Handle server response
+            handler = self.event_handler[response['action']]
+            new_content = handler(response)
+            fid, frame = new_content['fid'], new_content['container_frame']
+
+            # Show applications
+            cv2.imshow(self.winname, frame)
+            cv2.setTrackbarPos(self.barname, self.winname, fid)
+
+            # Handling keyboard events
+            key = cv2.waitKey(1) & 0xff
+            self.keyboaord_handler(key)
+
+        cv2.destroyAllWindows()
+
+    def keyboaord_handler(self, key):
+        # When certain panel is in focused
+        # ====================================
+        if self.mode == App.OPERATION_MODE:
+            if key == kb.ESC:
+                self.focus_panel.focus = False
+                self.focus_panel = None
+                self.mode = App.SELECT_MODE
+                return
+
+        # Common key handler
+        # =====================================
+        super().keyboaord_handler(key)
+
+    def mouse_callback(self, event, x, y, flags, param):
+        # Wait for selecting panel to focus on
+        # ==================================================
+        if self.mode == App.SELECT_MODE:
+            super().mouse_callback(event, x, y, flags, param)
+
+        elif self.mode == App.OPERATION_MODE:
+            pass
+
+    def trackbar_callback(self, value):
+        super().trackbar_callback(value)
+
+    def _track_handler(self, response):
+        # Rerender panels (add tracks)
+        panel_contents = []
+        for panel in response['content']:
+            pid = panel['pid']
+            tids = [ track['tid'] for track in panel['tracks'] ]
+            bboxes = [ track['bbox'] for track in panel['tracks'] ]
+            covars = [ track['covar'] for track in panel['tracks'] ]
+
+            # Select target panel to manipulate
+            target_panel = [ panel
+                            for panel in self.panels
+                            if panel.pid == pid ][0]
+
+            # Convert coordinate system
+            target_media_frame = target_panel.media_cache
+            new_resolution = target_media_frame.shape[:2][::-1]
+            old_resolution = self.trans_resolution
+
+            bboxes = convert_bbox_coordinate(bboxes, old_resolution, new_resolution)
+            means = np.array([ ((b[0]+b[2])//2, (b[1]+b[3])//2) for b in bboxes ])
+
+            scale_vec = np.array(new_resolution) / np.array(old_resolution)
+            covars = np.array(covars)*scale_vec
+
+            # Draw tracks on target panel
+            for tid, bbox, mean, covar in zip(tids, bboxes, means, covars):
+                bbox_color = get_unique_color(tid)
+                draw_bbox(target_media_frame,
+                            bbox=bbox,
+                            color=(bbox_color),
+                            thickness=3)
+                draw_text(target_media_frame,
+                            text=str(tid),
+                            position=bbox[:2],
+                            fontScale=3,
+                            fgcolor=(255, 255, 255),
+                            bgcolor=bbox_color)
+                draw_gaussian(target_media_frame,
+                            mean=mean,
+                            covariance=covar,
+                            color=bbox_color)
+
+            # Rerender
+            target_panel_content = target_panel.rerender(target_media_frame)
+            panel_contents.append(target_panel_content)
+
+        # Align/Sort rerendered panel_contents
+        panel_contents = [ [ panel_content
+                            for panel_content in panel_contents
+                            if panel_content['pid'] == panel.pid ][0]
+                        for panel in self.panels ]
+
+        # Rerender container
+        content = self.rerender(panel_contents)
+        return content
--- app/utils/visualize.py
+++ app/utils/visualize.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-
+import colorsys
 
 def draw_bbox(frame, bbox, color=(85,135,255), thickness=2):
     """Draw bounding box on the specified frame
@@ -14,3 +14,67 @@ def draw_bbox(frame, bbox, color=(85,135,255), thickness=2):
     tl_x, tl_y = tuple([ int(v) for v in bbox[:2]])
     br_x, br_y = tuple([ int(v) for v in bbox[2:]])
     cv2.rectangle(frame, (tl_x, tl_y), (br_x, br_y), color, thickness)
+
+def draw_text(frame, text, position,
+            fgcolor=(85, 125, 255),
+            bgcolor=(85, 135, 255),
+            fontScale=1, thickness=2, margin=5):
+    """Draw text on the specified frame
+
+    Args:
+        frame (ndarray): processing frame
+        text (string): text to render
+        position (tuple): text position (tl_x, tl_y)
+        fgcolor (tuple): BGR color palette for font color
+        bgcolor (tuple): BGR color palette for background color
+        fontScale (int): font scale
+        thickness (int): line thickness
+        margin (int): space between texts
+    """
+    # opencv doesn't handle `\n` in the text
+    # therefore we handle it line by line
+    lines = text.split('\n')
+    text_widths = [ margin*2+cv2.getTextSize(text=line,
+                                    thickness=thickness,
+                                    fontScale=fontScale,
+                                    fontFace=cv2.FONT_HERSHEY_COMPLEX_SMALL)[0][0]
+                    for line in lines ]
+    text_heights = [ margin*2+cv2.getTextSize(text=line,
+                                    thickness=thickness,
+                                    fontScale=fontScale,
+                                    fontFace=cv2.FONT_HERSHEY_COMPLEX_SMALL)[0][1]
+                    for line in lines ]
+    max_width = int(max(text_widths))
+    max_height = int(max(text_heights))
+    tl_x = int(position[0])
+    tl_y = int(position[1])
+
+    # draw background
+    cv2.rectangle(frame,
+                (tl_x, tl_y),
+                (tl_x+max_width, tl_y+max_height*len(lines)),
+                bgcolor, -1)
+
+    # draw text line by line
+    for j, line in enumerate(lines):
+        cv2.putText(frame, line,
+                (tl_x+margin, tl_y+(max_height*(j+1))-margin),
+                color=fgcolor,
+                fontScale=fontScale,
+                thickness=thickness,
+                fontFace=cv2.FONT_HERSHEY_COMPLEX_SMALL)
+
+def draw_gaussian(frame, mean, covariance, color):
+    vals, vecs = np.linalg.eigh(5.9915 * covariance)
+    indices = vals.argsort()[::-1]
+    vals, vecs = np.sqrt(vals[indices]), vecs[:, indices]
+
+    center = int(mean[0] + .5), int(mean[1] + .5)
+    axes = int(vals[0] + .5), int(vals[1] + .5)
+    angle = int(180. * np.arctan2(vecs[1, 0], vecs[0, 0]) / np.pi)
+    cv2.ellipse(frame, center, axes, angle, 0, 360, color, 2)
+
+def get_unique_color(tag, hue_step=0.41):
+    h, v = (tag*hue_step) % 1, 1. - (int(tag*hue_step)%4)/5.
+    r, g, b = colorsys.hsv_to_rgb(h, 1., v)
+    return int(r*255), int(255*g), int(255*b)
